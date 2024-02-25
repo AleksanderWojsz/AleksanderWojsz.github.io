0043fa442977a4002025d4c9268bbeca7636bb03
AleksanderWojsz <aw450252@students.mimuw.edu.pl>
2024-02-25 20:35:27 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+from bs4 import BeautifulSoup
+import requests
+
+html_text = requests.get("https://en.wikipedia.org/wiki/List_of_most_visited_palaces_and_monuments").text
+soup = BeautifulSoup(html_text, "lxml")
+
+full_table = soup.find("table", class_="wikitable sortable")
+rows = full_table.find_all("tr")
+
+with open("index.md", "w", encoding="utf-8") as file:  # Otwiera plik w trybie zapisu
+    file.write(rows[0].text.strip() + "\n\n")
+    file.write("| Position |")
+    file.write(" | ".join(rows[1].text.strip().replace("\n", ", ").split(", ")[
+                          :3]) + "|\n")  # Łączy trzy pierwsze elementy odzielając je przecinkami
+    file.write("| --- | --- | --- | --- |\n")
+
+    position = 1  # Inicjalizuje zmienną licznikową dla pozycji
+
+    for row in rows[2:]:  # pomija nagłówek
+        cells = row.find_all("td")
+
+        # Dodaje pozycję jako pierwszą komórkę w każdym wierszu
+        file.write(f"| {position}")
+
+        for i in range(3):
+            file.write(" | ")
+            if i == 1:  # czy to kolumna z linkiem do miasta
+                city_links = cells[i].find_all("a")  # Wyszukuje wszystkie linki w komórce
+
+                country_flag = city_links[0].find("img")  # Znajduje obraz flagi kraju
+                country_flag_url = "https:" + country_flag.get("src")  # Pobiera adres URL flagi
+                file.write(f"![Flaga]({country_flag_url}) ")  # Zapisuje obraz flagi
+
+                country_name = city_links[0].get("title")  # Pobiera nazwę kraju
+                country_url = "https://en.wikipedia.org" + city_links[0].get("href")  # Pobiera adres URL kraju
+                file.write(f"[{country_name}]({country_url}), ")  # Zapisuje nazwę kraju jako link
+
+                city_name = city_links[1].text.strip()  # Pobiera nazwę miasta
+                city_url = "https://en.wikipedia.org" + city_links[1].get("href")  # Pobiera adres URL miasta
+                file.write(f"[{city_name}]({city_url})")  # Zapisuje nazwę miasta jako link
+            else:
+                file.write(cells[i].text.strip())
+
+        position += 1  # Zwiększa licznik pozycji
+        file.write("\n")
